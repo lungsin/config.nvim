2a19fd1cd1314e481131971d58967251ef31989a
Kwee Lung Sin <kweelungsin@gmail.com>
2024-09-01 10:19:11 +0700

    install mini.ai

--- lua/klungs/plugins/mini.lua
+++ lua/klungs/plugins/mini.lua
@@ -2,6 +2,7 @@ return {
   'echasnovski/mini.nvim',
   version = false,
   config = function()
+    require('mini.ai').setup()
     -- require('mini.extra').setup()
     -- require('mini.pick').setup()
   end,
