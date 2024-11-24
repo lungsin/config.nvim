1a1e7fccaf087555392d2cef180fdade94cc7e90
Kwee Lung Sin <kweelungsin@gmail.com>
2024-11-24 23:15:50 +0800

    Fix: add filetypes for clangd

--- lua/klungs/plugins/lsp/lspconfig.lua
+++ lua/klungs/plugins/lsp/lspconfig.lua
@@ -119,7 +119,9 @@ return { -- LSP Configuration & Plugins
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      clangd = {},
+      clangd = {
+        filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'hpp' },
+      },
       gopls = {},
       -- pyright = {},
       rust_analyzer = {},
