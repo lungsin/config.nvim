da104c199894cd27ebbdda97e5684530a6ebaf50
Kwee Lung Sin <kweelungsin@gmail.com>
2024-12-06 00:58:08 +0800

    Enable fterm

--- lua/klungs/plugins/fterm.lua
+++ lua/klungs/plugins/fterm.lua
@@ -1,11 +1,15 @@
 return {
   'numToStr/FTerm.nvim',
-  enabled = false,
   config = function()
     -- local fterm = require('FTerm')
     -- fterm.setup({})
 
-    vim.keymap.set('n', '<C-_>', '<CMD>lua require("FTerm").toggle()<CR>')
-    vim.keymap.set('t', '<C-_>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+    vim.keymap.set('n', '<C-i>', '<CMD>lua require("FTerm").toggle()<CR>', { desc = 'Fterm: Toggle Terminal' })
+    vim.keymap.set(
+      't',
+      '<C-i>',
+      '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>',
+      { desc = 'Fterm: Toggle Terminal' }
+    )
   end,
 }
