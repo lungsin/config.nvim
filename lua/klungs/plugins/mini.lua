3d155c4bd2044429598be2fe6779e39353485eb0
Kwee Lung Sin <kweelungsin@gmail.com>
2024-12-09 14:01:04 +0800

    Nit: Change keymap for fterm and mini.ai @loop

--- lua/klungs/plugins/mini.lua
+++ lua/klungs/plugins/mini.lua
@@ -12,7 +12,7 @@ return {
         N = extra.gen_ai_spec.number(),
         F = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }),
         A = ai.gen_spec.treesitter({ a = '@parameter.outer', i = '@parameter.inner' }),
-        L = ai.gen_spec.treesitter({ a = '@loop.outer', i = '@loop.inner' }),
+        l = ai.gen_spec.treesitter({ a = '@loop.outer', i = '@loop.inner' }),
         c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }),
         e = ai.gen_spec.treesitter({ a = '@assignment.rhs', i = '@assignment.lhs' }),
         E = ai.gen_spec.treesitter({ a = '@assignment.outer', i = '@assignment.inner' }),
