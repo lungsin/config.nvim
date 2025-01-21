5e4d3b982aaa70223c892198a5afa8429d380d40
Kwee Lung Sin <kweelungsin@gmail.com>
2025-01-21 18:27:35 +0800

    blink.cmp: add keymap for going next/prev in snippet argument

--- lua/klungs/plugins/lsp/blink.lua
+++ lua/klungs/plugins/lsp/blink.lua
@@ -17,7 +17,11 @@ return {
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- see the "default configuration" section below for full documentation on how to define
     -- your own keymap.
-    keymap = { preset = 'default' },
+    keymap = {
+      preset = 'default',
+      ['<C-l>'] = { 'snippet_forward', 'fallback' },
+      ['<C-h>'] = { 'snippet_backward', 'fallback' },
+    },
 
     appearance = {
       -- Sets the fallback highlight groups to nvim-cmp's highlight groups
