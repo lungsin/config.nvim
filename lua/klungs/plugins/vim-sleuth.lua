59625557fc53e0150f5e8db666e0d56400b51748
Kwee Lung Sin <kweelungsin@gmail.com>
2025-02-06 13:08:57 +0800

    vim-sleuth: fix conflicting keymap with trouble.nvim

--- lua/klungs/plugins/vim-sleuth.lua
+++ lua/klungs/plugins/vim-sleuth.lua
@@ -1,6 +1,6 @@
 return {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   config = function()
-    vim.keymap.set('n', '<leader>ts', '<cmd>Sleuth<cr>', { desc = 'Trigger sleuth: adjust indent width' })
+    vim.keymap.set('n', '<leader>tS', '<cmd>Sleuth<cr>', { desc = 'Trigger sleuth: adjust indent width' })
   end,
 }
