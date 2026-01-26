5be091ee78027131b221b4853b7172be0fd1cdeb
Kwee Lung Sin <klungs.dev@gmail.com>
2026-01-27 03:19:15 +0700

    Add jai-format to conform

--- lua/klungs/plugins/conform.lua
+++ lua/klungs/plugins/conform.lua
@@ -22,57 +22,73 @@ return { -- Autoformat
       desc = 'Toggle Auto Format',
     },
   },
-  ---@type conform.setupOpts
-  opts = {
-    notify_on_error = false,
-    format_on_save = function(bufnr)
-      -- Check if auto-format is disabled for the current buffer
-      if vim.b[bufnr].disable_autoformat then
-        return
-      end
-      -- Disable if it's QMK specific keymap.c file, to avoid format conflict with qmk.nvim plugin
-      local buf_name = vim.api.nvim_buf_get_name(bufnr)
-      if buf_name:match('keymap.c$') then
-        return
-      end
+  config = function()
+    require('conform').setup({
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Check if auto-format is disabled for the current buffer
+        if vim.b[bufnr].disable_autoformat then
+          return
+        end
+        -- Disable if it's QMK specific keymap.c file, to avoid format conflict with qmk.nvim plugin
+        local buf_name = vim.api.nvim_buf_get_name(bufnr)
+        if buf_name:match('keymap.c$') then
+          return
+        end
 
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes =
-        { c = true, cpp = true, javascript = true, typescript = true, typescriptreact = true, javascriptreact = true }
-      local lsp_format_opt
-      if disable_filetypes[vim.bo[bufnr].filetype] then
-        lsp_format_opt = 'never'
-      else
-        lsp_format_opt = 'fallback'
-      end
-      return {
-        timeout_ms = 500,
-        lsp_format = lsp_format_opt,
-      }
-    end,
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      -- For FE stuff, we use rely on eslint instead. It's being setup on
-      -- the lsp side.
-      -- NOTE: Somehow the prettier formatter is quite slow and the prettierd
-      -- formatter is conflicting with the lsp code action due to it being async
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes =
+          { c = true, cpp = true, javascript = true, typescript = true, typescriptreact = true, javascriptreact = true }
+        local lsp_format_opt
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          lsp_format_opt = 'never'
+        else
+          lsp_format_opt = 'fallback'
+        end
+        return {
+          timeout_ms = 500,
+          lsp_format = lsp_format_opt,
+        }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- For FE stuff, we use rely on eslint instead. It's being setup on
+        -- the lsp side.
+        -- NOTE: Somehow the prettier formatter is quite slow and the prettierd
+        -- formatter is conflicting with the lsp code action due to it being async
 
-      -- javascript = { 'prettierd', 'prettier', stop_after_first = true },
-      -- typescript = { 'prettierd', 'prettier', stop_after_first = true },
-      -- typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-      -- javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-      json = { 'prettierd', 'prettier', stop_after_first = true },
-      rust = { 'rustfmt' },
-      go = { 'goimports' },
-      c = { 'clang_format' },
-      cpp = { 'clang_format' },
-    },
-    formatters = {
-      clang_format = {
-        prepend_args = { '--style=file', '--fallback-style=LLVM' },
+        -- javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        -- typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        -- typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        -- javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
+        rust = { 'rustfmt' },
+        go = { 'goimports' },
+        c = { 'clang_format' },
+        cpp = { 'clang_format' },
+        jai = function(bufnr)
+          -- Don't enable formatter if it's inside the compiler/stdlib path.
+          local jai_path = vim.env.JAI_PATH or vim.fn.expand('~/jai/jai')
+          local filename = vim.api.nvim_buf_get_name(bufnr)
+          if vim.startswith(filename, jai_path) then
+            return {}
+          end
+          return { 'jai-format' }
+        end,
       },
-    },
-  },
+      formatters = {
+        clang_format = {
+          prepend_args = { '--style=file', '--fallback-style=LLVM' },
+        },
+        ['jai-format'] = {
+          command = 'jai-format',
+          args = { '-silent', '-to_file', '$FILENAME' },
+          cwd = require('conform.util').root_file({ '.jai-format' }),
+          stdin = false,
+        },
+      },
+    })
+  end,
 }
