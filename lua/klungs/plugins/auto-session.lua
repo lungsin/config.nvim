cb4fc5b0e0e9844a9bc4378edfe742e205e18e88
Kwee Lung Sin <klungs.dev@gmail.com>
2026-04-30 13:45:08 +0700

    Add quicker.nvim

--- lua/klungs/plugins/auto-session.lua
+++ lua/klungs/plugins/auto-session.lua
@@ -2,11 +2,11 @@ return {
   'rmagatti/auto-session',
   lazy = false,
   keys = {
-    { '<leader>qR', '<cmd>AutoSession restore<CR>', desc = 'Restore session' },
-    { '<leader>qd', '<cmd>AutoSession delete<CR>', desc = 'Delete session' },
-    { '<leader>qs', '<cmd>AutoSession search<CR>', desc = 'Search session' },
-    { '<leader>qr', '<cmd>restart<CR>', desc = 'Restart' },
-    { '<leader>qq', '<cmd>wqa<CR>', desc = 'Quit' },
+    { '<leader>Qr', '<cmd>AutoSession restore<CR>', desc = 'Restore session' },
+    { '<leader>Qd', '<cmd>AutoSession delete<CR>', desc = 'Delete session' },
+    { '<leader>Qs', '<cmd>AutoSession search<CR>', desc = 'Search session' },
+    { '<leader>Qr', '<cmd>restart<CR>', desc = 'Restart' },
+    { '<leader>Qq', '<cmd>wqa<CR>', desc = 'Quit' },
   },
   opts = {
     log_level = 'error',
---
+++ lua/klungs/plugins/quicker.lua
@@ -0,0 +1,26 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  'stevearc/quicker.nvim',
+  ft = 'qf',
+  ---@module "quicker"
+  ---@type quicker.SetupOptions
+  opts = {
+    keys = {
+      {
+        '>',
+        function() require('quicker').expand({ before = 2, after = 2, add_to_existing = true }) end,
+        desc = 'Expand quickfix context',
+      },
+      {
+        '<',
+        function() require('quicker').collapse() end,
+        desc = 'Collapse quickfix context',
+      },
+    },
+  },
+  keys = {
+    { '<leader>q', function() require('quicker').toggle() end, desc = 'Toggle quickfix' },
+    { '<leader>l', function() require('quicker').toggle({}) end, desc = 'Toggle loclist' },
+  },
+}
